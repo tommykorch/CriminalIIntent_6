3ee80da386a27b8818f32706ac9562551c76a41a
tommykorch <tkorchmit@bk.ru>
2024-12-17 21:20:14 +0700

    Part 3

---
+++ app/src/main/java/com/sample/criminaliintent/database/CrimeDatabase.kt
@@ -0,0 +1,12 @@
+package com.sample.criminaliintent.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.sample.criminaliintent.Crime
+
+@Database(entities = [ Crime::class ], version=1)
+@TypeConverters(CrimeTypeConverters::class)
+abstract class CrimeDatabase : RoomDatabase() {
+    abstract fun crimeDao(): CrimeDao
+}
