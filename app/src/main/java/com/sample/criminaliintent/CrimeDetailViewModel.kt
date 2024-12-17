8c7b3384c644db8bc0eeebba4fe0bc28e93c6c8e
tommykorch <tkorchmit@bk.ru>
2024-12-17 23:29:44 +0700

    Part 5

---
+++ app/src/main/java/com/sample/criminaliintent/CrimeDetailViewModel.kt
@@ -0,0 +1,21 @@
+package com.sample.criminaliintent
+
+import android.util.Log
+import androidx.lifecycle.*
+import androidx.lifecycle.Observer
+import java.util.*
+
+class CrimeDetailViewModel: ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+    private val crimeIdLiveData = MutableLiveData<UUID>()
+    var crimeLiveData: LiveData<Crime?> =
+        crimeIdLiveData.switchMap { crimeId ->
+            crimeRepository.getCrime(crimeId)
+        }
+    fun loadCrime(crimeId: UUID) {
+        crimeIdLiveData.value = crimeId
+    }
+    fun saveCrime(crime: Crime) {
+        crimeRepository.updateCrime(crime)
+    }
+}
---
+++ app/src/main/java/com/sample/criminaliintent/CrimeListViewModel.kt
@@ -0,0 +1,11 @@
+package com.sample.criminaliintent
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel: ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
+    fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
+    }
+}
